06ff8002a38aa455e0763007e9ae62c465649786
Agent-11 <techgod18@outlook.com>
2021-07-04 08:41:54 -0400

    Code cleanup

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
-//Many thanks to https://github.com/juniorxsound/THREE.Multiplayer for the base code, I gotta credit them for the work they did.
+//Credits to https://github.com/juniorxsound/THREE.Multiplayer for the base code, I gotta credit them for the work they did.
 'use strict'
 
 //Port and server setup
-const port = 3000;
+const port = 3000
 const clientPath = "http://localhost:8080"
 
 //Console the port
-console.log('Server is running localhost on port: ' + port );
+console.log('Server is running locally on port: ' + port )
 
-/////SOCKET.IO///////
 const io = require('socket.io')(port, { cors: { origin: [clientPath] } })
-
 let clients = {}
 
-//Socket setup
 io.on('connection', client=>{
-
-  console.log('User ' + client.id + ' connected, there are ' + io.engine.clientsCount + ' clients connected');
+  console.log('User ' + client.id + ' connected, there are ' + io.engine.clientsCount + ' clients connected')
 
   //Add a new client indexed by his id
   clients[client.id] = {
@@ -25,28 +21,28 @@ io.on('connection', client=>{
   }
 
   //Make sure to send the client it's ID
-  client.emit('introduction', client.id, io.engine.clientsCount, Object.keys(clients));
+  client.emit('introduction', client.id, io.engine.clientsCount, Object.keys(clients))
 
   //Update everyone that the number of users has changed
-  io.sockets.emit('newUserConnected', io.engine.clientsCount, client.id, Object.keys(clients));
+  io.sockets.emit('newUserConnected', io.engine.clientsCount, client.id, Object.keys(clients))
 
   client.on('move', (pos)=>{
 
-    clients[client.id].position = pos;
-    io.sockets.emit('userPositions', clients);
+    clients[client.id].position = pos
+    io.sockets.emit('userPositions', clients)
 
-  });
+  })
 
   //Handle the disconnection
   client.on('disconnect', ()=>{
 
     //Delete this client from the object
-    delete clients[client.id];
+    delete clients[client.id]
 
-    io.sockets.emit('userDisconnected', io.engine.clientsCount, client.id, Object.keys(clients));
+    io.sockets.emit('userDisconnected', io.engine.clientsCount, client.id, Object.keys(clients))
 
-    console.log('User ' + client.id + ' disconnected, there are ' + io.engine.clientsCount + ' clients connected');
+    console.log('User ' + client.id + ' disconnected, there are ' + io.engine.clientsCount + ' clients connected')
 
-  });
+  })
 
-});
+})
