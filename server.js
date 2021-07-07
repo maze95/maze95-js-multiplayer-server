67fd7dd4c16820a32cdb2ede304f3118c829bf54
Agent-11 <techgod18@outlook.com>
2021-07-06 22:11:33 -0400

    Player limit implementation and small bug fix

--- server.js
+++ server.js
@@ -9,18 +9,23 @@ const clientPath = "http://localhost:8080"
 console.log('Server is running locally on port: ' + port )
 
 const io = require('socket.io')(port, { cors: { origin: [clientPath] } })
+const twoPlayerLimit = false
 let clients = {}
 
 io.on('connection', client=>{
   console.log('User ' + client.id + ' connected, there are ' + io.engine.clientsCount + ' clients connected')
+  if(io.engine.clientsCount == 3 && twoPlayerLimit) {
+    //client.disconnect()
+    client.emit('selfDisconnect', client.id)
+  }
 
-  //Add a new client indexed by his id
+  //Add a new client indexed by their id
   clients[client.id] = {
-    position: [0, 0, 0],
+    position: [0, -7, -23],
     rotation: [0, 0, 0]
   }
 
-  //Make sure to send the client it's ID
+  //Make sure to send the client their ID
   client.emit('introduction', client.id, io.engine.clientsCount, Object.keys(clients))
 
   //Update everyone that the number of users has changed
